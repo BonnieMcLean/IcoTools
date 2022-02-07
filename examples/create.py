8a5a23aadce1fc312b3ba328ff0f767c92f3828a
Bonnie McLean <59.b.mclean@gmail.com>
2022-02-07 14:19:57 +0100

    added support for images

--- examples/create.py
+++ examples/create.py
@@ -2,31 +2,35 @@ import icotools
 
 # mp4 stimuli, guessing experiments
 
-icotools.guesser('mp4stimuli_randomfoils.csv','gestures_custom.csv')
-icotools.guesser('mp4stimuli_randomfoils.csv','gestures_default.csv')
+icotools.guesser('02-mp4stimuli_randomfoils.csv','gestures_custom.csv')
+icotools.guesser('02-mp4stimuli_randomfoils.csv','gestures_default.csv')
 
-icotools.guesser('mp4stimuli_randomfoils.csv','JSL_custom.csv')
-icotools.guesser('mp4stimuli_randomfoils.csv','JSL_default.csv')
+icotools.guesser('02-mp4stimuli_randomfoils.csv','JSL_custom.csv')
+icotools.guesser('02-mp4stimuli_randomfoils.csv','JSL_default.csv')
 
 # mp4 stimuli, rating experiments
 
-icotools.rater('mp4stimuli_randomfoils.csv','gestures_custom.csv')
-icotools.rater('mp4stimuli_randomfoils.csv','gestures_default.csv')
+icotools.rater('02-mp4stimuli_randomfoils.csv','gestures_custom.csv')
+icotools.rater('02-mp4stimuli_randomfoils.csv','gestures_default.csv')
 
-icotools.rater('mp4stimuli_randomfoils.csv','JSL_custom.csv')
-icotools.rater('mp4stimuli_randomfoils.csv','JSL_default.csv')
+icotools.rater('02-mp4stimuli_randomfoils.csv','JSL_custom.csv')
+icotools.rater('02-mp4stimuli_randomfoils.csv','JSL_default.csv')
 
 # mp3 stimuli, guessing experiments
 
-icotools.guesser('mp3stimuli_oppositefoils.csv','Korean_custom.csv')
-icotools.guesser('mp3stimuli_oppositefoils.csv','Korean_default.csv')
+icotools.guesser('01-mp3stimuli_oppositefoils.csv','Korean_custom.csv')
+icotools.guesser('01-mp3stimuli_oppositefoils.csv','Korean_default.csv')
 
-icotools.guesser('mp3stimuli_oppositefoils.csv','Nonwords_default.csv')
+icotools.guesser('01-mp3stimuli_oppositefoils.csv','Nonwords_default.csv')
 
 # mp3 stimuli, rating experiments
 
-icotools.rater('mp3stimuli_oppositefoils.csv','Korean_custom.csv')
-icotools.rater('mp3stimuli_oppositefoils.csv','Korean_default.csv')
+icotools.rater('01-mp3stimuli_oppositefoils.csv','Korean_custom.csv')
+icotools.rater('01-mp3stimuli_oppositefoils.csv','Korean_default.csv')
 
 
-icotools.rater('mp3stimuli_oppositefoils.csv','Nonwords_default.csv')
+icotools.rater('01-mp3stimuli_oppositefoils.csv','Nonwords_default.csv')
+
+# image stimuli, guessing and rating experiments
+icotools.guesser('02-imagestimuli_randomfoils.csv','images_custom.csv')
+icotools.rater('02-imagestimuli_randomfoils.csv','images_custom.csv')
